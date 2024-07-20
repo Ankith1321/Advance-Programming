29ea2ccd673cc32f1de554ba86eedcb6b1449600
Ankith1321 <ankithreddy580@gmail.com>
2024-07-20 14:06:25 +0200

    update mimic.py
    update and change the code with the required fields and run the code

--- basic/mimic.py
+++ basic/mimic.py
@@ -48,13 +48,41 @@ import sys
 def mimic_dict(filename):
   """Returns mimic dict mapping each word to list of words which follow it."""
   # +++your code here+++
-  return
+  # LAB(begin solution)
+  mimic_dict = {}
+  f = open(filename, encoding='utf-8')
+  text = f.read()
+  f.close()
+  words = text.split()
+  prev = ''
+  for word in words:
+    if not prev in mimic_dict:
+      mimic_dict[prev] = [word]
+    else:
+      mimic_dict[prev].append(word)
+    # Could write as: mimic_dict[prev] = mimic_dict.get(prev, []) + [word]
+    # It's one line, but not totally satisfying.
+    prev = word
+  return mimic_dict
+  # LAB(replace solution)
+  # return
+  # LAB(end solution)
 
 
 def print_mimic(mimic_dict, word):
   """Given mimic dict and start word, prints 200 random words."""
   # +++your code here+++
-  return
+  # LAB(begin solution)
+  for unused_i in range(200):
+    print(word, end=' ')
+    nexts = mimic_dict.get(word)          # Returns None if not found
+    if not nexts:
+      nexts = mimic_dict['']  # Fallback to '' if not found
+    word = random.choice(nexts)
+  # The 'unused_' prefix turns off the lint warning about the unused variable.
+  # LAB(replace solution)
+  # return
+  # LAB(end solution)
 
 
 # Provided main(), calls mimic_dict() and mimic()
